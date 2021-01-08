75b8e4251d49289c50a0944d3121ecbff36bba69
eswarclynn <esuclintblazer@gmail.com>
2021-01-08 15:57:44 +0530

    Filter student attendance through years.

--- students/static/students/attendance_history.js
+++ students/static/students/attendance_history.js
@@ -1,26 +1,19 @@
 var temp_present_dates = present_dates.sort((a, b) => a - b);
 var temp_absent_dates = absent_dates.sort((a, b) => a - b);
 
-$(document).ready(function() {
-  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-  
-  monthNames.forEach((month, index) => {
-    let option = document.createElement("option");
-    option.innerText = month;
-    option.value = index;
-    document.querySelector('#select_month').appendChild(option);
-  });
-
+window.onload = function() {
   displayDates();
-});
+}
 
-function selectMonth(event) {
-  if(event.target.value == '') {
+function selectMonth(selectedDates, dateStr, instance) {
+  const value = dateStr;
+  const [month, year] = value.split("-").map(item => parseInt(item));
+  if(value == '') {
     temp_present_dates = present_dates;
     temp_absent_dates = absent_dates;
   } else {
-    temp_present_dates = present_dates.filter(item => item.getMonth() == event.target.value);
-    temp_absent_dates = absent_dates.filter(item => item.getMonth() == event.target.value);
+    temp_present_dates = present_dates.filter(date => date.getMonth() + 1 == month && date.getFullYear() == year);
+    temp_absent_dates = absent_dates.filter(date => date.getMonth() + 1 == month && date.getFullYear() == year);
   }
   displayDates();
 }
